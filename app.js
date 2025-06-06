28868f6c69a7bd181a341e524f34796ad34f1f6a
muhammaduzair432 <mikykhan702@gmail.com>
2025-06-06 15:42:46 +0500

    Add functionality for contact section, including smooth scrolling and email sending; implement popups for success and failure messages

--- app.js
+++ app.js
@@ -36,3 +36,93 @@ closeIcon.addEventListener("click", function () {
   sideBar.classList.remove("open-sidebar");
   sideBar.classList.add("close-sidebar");
 });
+
+// target the contact me button in hero section //
+const contactMeBtn = document.querySelector(".hero button");
+
+// function to scroll to contact section //
+function scrolltoContactSection() {
+  const contactSection = document.getElementById("contact-section");
+  contactSection.scrollIntoView({ behavior: "smooth" });
+}
+
+// add event listener to scroll to contact section //
+contactMeBtn.addEventListener("click", scrolltoContactSection);
+
+// target the contact me button in about section //
+const contactMeBtnAbout = document.querySelector(".info-section button");
+
+// add event listener to scroll to contact section //
+contactMeBtnAbout.addEventListener("click", scrolltoContactSection);
+
+// target the skill Sprint Project Button //
+const skillSprintBtn = document.getElementById("skillSprintProject");
+
+// add event listener to redirect to skill sprint project website //
+skillSprintBtn.addEventListener("click", () => {
+  redirectToProjectWebsite("https://mjskillsprint.vercel.app/");
+});
+
+// target the career path finder project button //
+const careerPathFinderBtn = document.getElementById("careerPathFinderProject");
+
+// add event listener to redirect to career path finder project website //
+careerPathFinderBtn.addEventListener("click", () => {
+  redirectToProjectWebsite("https://mjcareerpath.vercel.app/");
+});
+
+// target the e-commerce shop.co project button //
+const eCommerceShopBtn = document.getElementById("shopCoProject");
+
+// add event listener to redirect to e-commerce shop.co project website //
+eCommerceShopBtn.addEventListener("click", () => {
+  redirectToProjectWebsite("https://mjshopco.vercel.app/");
+});
+
+// function to redirect to skill sprint project //
+function redirectToProjectWebsite(link) {
+  window.open(link, "_blank");
+}
+
+// Function to send email using EmailJS //
+function sendEmail(name, email, message) {
+  emailjs
+    .send(
+      "service_iu0pbcp",
+      "template_dkid99g",
+      {
+        name: name,
+        email: email,
+        message: message,
+      },
+      "ART4tZD9aS_voEnoJ"
+    )
+    .then(
+      (response) => {
+        document.querySelector(".success-popup").style.display = "block"; // Show success popup
+        setTimeout(() => {
+          document.querySelector(".success-popup").style.display = "none"; // Hide success popup after 3 seconds
+        }, 1500);
+      },
+      (err) => {
+        document.querySelector(".failed-popup").style.display = "block"; // Show error popup
+        setTimeout(() => {
+          document.querySelector(".failed-popup").style.display = "none"; // Hide error popup after 3 seconds
+        }, 1500);
+      }
+    );
+}
+
+// target the contact form submit button //
+const contactFormSubmitBtn = document.querySelector(".contact-box button");
+
+// add event listener to send email on form submit //
+contactFormSubmitBtn.addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  const name = document.getElementById("fullName").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  sendEmail(name, email, message); // Call the sendEmail function
+});
